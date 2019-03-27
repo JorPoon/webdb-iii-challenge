a7b70ea2646fece764c61417ec93815a19a51f48
Jor_Poon <jormpoon@gmail.com>
2019-03-27 18:12:02 -0400

    created all endpoints for cohorts

--- routers/cohorts-router.js
+++ routers/cohorts-router.js
@@ -52,6 +52,38 @@ router.post('/', (req,res) => {
         });
 });
 
+router.put('/:id', (req, res) => {
+    db('cohorts')
+        .where({id: req.params.id})
+        .update(req.body)
+        .then(count => {
+            if (count > 0 ) {
+                res.status(200).json(count)
+            } else {
+                res.status(404).json({message: 'Cohort not found'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    db('cohorts')
+        .where({id: req.params.id})
+        .del()
+        .then(count => {
+            if(count > 0) {
+                res.status(200).end();
+            } else {
+                res.status(404).json({message: 'Cohort not found'})
+            }
+        }) 
+        .catch( err => {
+            res.status(500).json(err)
+        });
+});
+
 
 
 module.exports = router;
